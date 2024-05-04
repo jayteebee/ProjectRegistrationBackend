687bbf9bd9eae6a68b79ab32565827589d957850
Jethro Block <jethroblock22@gmail.com>
2024-05-04 09:51:48 +0100

    Add POST request validation and JSON body parsing

--- netlify/functions/handle-email/handle-email.js
+++ netlify/functions/handle-email/handle-email.js
@@ -1,17 +1,25 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    // Return a 405 Method Not Allowed response if the function is not called with a POST request
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+      headers: { "Allow": "POST" }
+    };
+  }
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    // Parse the JSON body from the request
+    const data = JSON.parse(event.body);
+    const subject = data.name || 'World';
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    };
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
-module.exports = { handler }
+module.exports = { handler };
